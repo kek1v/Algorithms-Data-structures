ab51c954ebc651bce38086907414665df1f4156f
kek1v <daddadov545@gmail.com>
2025-02-18 21:50:19 +0500

    мфти про расчет комбинаций из n по m

--- DynamicalProgramming/combinations.cpp
+++ DynamicalProgramming/combinations.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<vector>
 
-
 uint64_t combinations_recursive(int i,int j){
     if(i == 1 or j == 1){// base case
         return 1;
@@ -39,15 +38,34 @@ uint64_t combinations_dynamic(int n,int m){
 
 // c++ style
 uint64_t combinations_dynamic2(uint16_t n, uint16_t m){
-    // FIX ME
-    return 0;
+    std::vector<std::vector<uint64_t>> K;
+
+    // memory allocation for K function
+    // resize - устанавливает размер вектора
+    K.resize(n + 1);
+    for(int i = 0; i <= n; i++)
+        K[i].resize(m + 1);
+
+    // base cases
+    for(int i = 1; i <= n; i++)
+        K[i][1] = 1;
+    for(int j = 1; j <= m; j++)
+        K[1][j] = 1;
+
+    // recursive case
+    for(int i = 2; i <= n; i++){
+        for(int j = 2; j <= m; j++)
+            K[i][j] = K[i - 1][j] + K[i][j - 1]; // our formula
+    }
+    
+    return K[n][m];
 }
 
 int main(){
     // программе нужно заранее знать количество строк
-    const uint16_t N = 50;
-    const uint16_t M = 100;
-    std::cout<<"Number of ways to reach the destination is: "<<combinations_dynamic(N,M)<<std::endl;
+    const uint16_t N = 500;
+    const uint16_t M = 1000;
+    std::cout<<"Number of ways to reach the destination is: "<<combinations_dynamic2(N,M)<<std::endl;
 
     return 0;
 }
