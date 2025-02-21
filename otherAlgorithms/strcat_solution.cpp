de177b0756d9a76e7214097b1d14792820a7c87f
kek1v <daddadov545@gmail.com>
2025-02-22 00:01:18 +0500

    мфти о решении проблемы си строк и strcat

---
+++ otherAlgorithms/strcat_solution.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+// Решение задачи "Склеивание строк"
+// Время работы: O(n) в отличии от O(n^2) в случае с простым сложением строк в си
+
+int main(){
+    std::string hello = "Hello, world! ";
+    std::string result;
+    std::cout<< hello << std::endl;
+
+    int times_to_concatenate;
+    std::cin >> times_to_concatenate;
+    for(int i = 0; i < times_to_concatenate; i++){
+        result += hello;
+    } 
+
+    std::cout << result << std::endl;
+
+    return 0;
+}
